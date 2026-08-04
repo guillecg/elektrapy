fc6f2350637c513790e0ada1e5fd8d57289557f4
guillecg <guillemcg92@gmail.com>
2026-08-04 15:54:20 +0200

    Rename results_df to df

--- src/elektrapy/preprocessing.py
+++ src/elektrapy/preprocessing.py
@@ -3,14 +3,16 @@ import pandas as pd
 from elektrapy import PATHWAY_NODES_MAP
 
 
-def _get_nodes(results_df: pd.DataFrame) -> pd.DataFrame:
+def _get_nodes(df: pd.DataFrame) -> pd.DataFrame:
     """
     Auxiliary function for parsing the results and getting both the source 
     and target nodes as well as their corresponding biogeochemical cycle.
     """
 
+    assert len(df), "[ERROR] empty dataframe"
+
     # Add cycle
-    results_df["cycle"] = results_df["pathway"]\
+    df["cycle"] = df["pathway"]\
         .str.split("-")\
         .str[0]\
         .map({
@@ -21,20 +23,20 @@ def _get_nodes(results_df: pd.DataFrame) -> pd.DataFrame:
         })
 
     # Add sources and targets (i.e. substrates and products)
-    results_df[["source", "target"]] = results_df["pathway"]\
+    df[["source", "target"]] = df["pathway"]\
         .replace(PATHWAY_NODES_MAP)\
         .str.split(" -> ", expand=True)
 
     # Drop pathways without sources or targets (i.e. not mapped)
-    results_df = results_df.dropna(subset="source")
-    results_df = results_df.dropna(subset="target")
+    df = df.dropna(subset="source")
+    df = df.dropna(subset="target")
 
     # Explode multiple sources (e.g. methanogenesis)
-    results_df["source"] = results_df["source"].str.split(";")
-    results_df = results_df.explode("source")
+    df["source"] = df["source"].str.split(";")
+    df = df.explode("source")
 
     # Explode multiple targets (e.g. from disproportionation)
-    results_df["target"] = results_df["target"].str.split(";")
-    results_df = results_df.explode("target")
+    df["target"] = df["target"].str.split(";")
+    df = df.explode("target")
 
-    return results_df
+    return df
