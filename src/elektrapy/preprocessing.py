1a89d6f445041d95a987ba16accb2d596da0b0ac
guillecg <guillemcg92@gmail.com>
2026-08-04 19:28:42 +0200

    Add initial preprocess_data function

--- src/elektrapy/preprocessing.py
+++ src/elektrapy/preprocessing.py
@@ -3,13 +3,31 @@ import pandas as pd
 from elektrapy import PATHWAY_NODES_MAP
 
 
+def preprocess_data(
+    df: pd.DataFrame,
+    group_var: str,
+    fn_var: str = "function"
+) -> pd.DataFrame:
+    """
+    Main function for preprocessing functional annotation data (bigecyhmm) 
+    and yield a formatted dataframe that can be used in electron flow diagrams.
+    """
+
+    assert len(df), "[ERROR] Empty dataframe."
+    assert fn_var in df.columns, f"[ERROR] Missing column '{fn_var}'."
+
+    df = df.rename(columns={fn_var: "pathway"})
+
+    return df
+
+
 def _get_nodes(df: pd.DataFrame) -> pd.DataFrame:
     """
     Auxiliary function for parsing the results and getting both the source 
     and target nodes as well as their corresponding biogeochemical cycle.
     """
 
-    assert len(df), "[ERROR] empty dataframe"
+    assert len(df), "[ERROR] Empty dataframe."
 
     # Add cycle
     df["cycle"] = df["pathway"]\
