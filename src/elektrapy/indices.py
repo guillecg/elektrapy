f2a28fe2af01d9c16d2c382d5f86c9aac4b3d4d9
guillecg <guillemcg92@gmail.com>
2026-08-06 09:37:56 +0200

    Move get_nodes to indices.py

---
+++ src/elektrapy/indices.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+def get_nodes(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Auxiliary function for parsing the results and getting both the source 
+    and target nodes as well as their corresponding biogeochemical cycle.
+    """
+
+    assert len(df), "[ERROR] Empty dataframe."
+
+    # Add cycle
+    df["cycle"] = df["pathway"]\
+        .str.split("-")\
+        .str[0]\
+        .map({
+            "C": "carbon",
+            "N": "nitrogen",
+            "O": "other",
+            "S": "sulfur"
+        })
+
+    # Add sources and targets (i.e. substrates and products)
+    df[["source", "target"]] = df["pathway"]\
+        .replace(PATHWAY_NODES_MAP)\
+        .str.split(" -> ", expand=True)
+
+    # Drop pathways without sources or targets (i.e. not mapped)
+    df = df.dropna(subset="source")
+    df = df.dropna(subset="target")
+
+    # Explode multiple sources (e.g. methanogenesis)
+    df["source"] = df["source"].str.split(";")
+    df = df.explode("source")
+
+    # Explode multiple targets (e.g. from disproportionation)
+    df["target"] = df["target"].str.split(";")
+    df = df.explode("target")
+
+    return df
