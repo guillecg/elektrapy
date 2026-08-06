2495de899557c04f208ce7f4537a0e25823b0a1b
guillecg <guillemcg92@gmail.com>
2026-08-06 11:48:30 +0200

    Reorganise functions within indices.py

--- src/elektrapy/indices.py
+++ src/elektrapy/indices.py
@@ -3,6 +3,45 @@ import pandas as pd
 from elektrapy import PATHWAY_NODES_MAP
 
 
+def _get_nodes(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Auxiliary function for parsing the results and getting both the source 
+    and target nodes as well as their corresponding biogeochemical cycle.
+    """
+
+    assert len(df), "[ERROR] Empty dataframe."
+
+    # Add cycle
+    df["cycle"] = df["pathway"]\
+        .str.split("-")\
+        .str[0]\
+        .map({
+            "C": "carbon",
+            "N": "nitrogen",
+            "O": "other",
+            "S": "sulfur"
+        })
+
+    # Add sources and targets (i.e. substrates and products)
+    df[["source", "target"]] = df["pathway"]\
+        .replace(PATHWAY_NODES_MAP)\
+        .str.split(" -> ", expand=True)
+
+    # Drop pathways without sources or targets (i.e. not mapped)
+    df = df.dropna(subset="source")
+    df = df.dropna(subset="target")
+
+    # Explode multiple sources (e.g. methanogenesis)
+    df["source"] = df["source"].str.split(";")
+    df = df.explode("source")
+
+    # Explode multiple targets (e.g. from disproportionation)
+    df["target"] = df["target"].str.split(";")
+    df = df.explode("target")
+
+    return df
+
+
 def get_redox_index(
     results_df: pd.DataFrame,
     group_var: str
@@ -69,45 +108,6 @@ def get_redox_index(
         .sort_values("redox_index")
 
     return rti_df
-
-
-def _get_nodes(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Auxiliary function for parsing the results and getting both the source 
-    and target nodes as well as their corresponding biogeochemical cycle.
-    """
-
-    assert len(df), "[ERROR] Empty dataframe."
-
-    # Add cycle
-    df["cycle"] = df["pathway"]\
-        .str.split("-")\
-        .str[0]\
-        .map({
-            "C": "carbon",
-            "N": "nitrogen",
-            "O": "other",
-            "S": "sulfur"
-        })
-
-    # Add sources and targets (i.e. substrates and products)
-    df[["source", "target"]] = df["pathway"]\
-        .replace(PATHWAY_NODES_MAP)\
-        .str.split(" -> ", expand=True)
-
-    # Drop pathways without sources or targets (i.e. not mapped)
-    df = df.dropna(subset="source")
-    df = df.dropna(subset="target")
-
-    # Explode multiple sources (e.g. methanogenesis)
-    df["source"] = df["source"].str.split(";")
-    df = df.explode("source")
-
-    # Explode multiple targets (e.g. from disproportionation)
-    df["target"] = df["target"].str.split(";")
-    df = df.explode("target")
-
-    return df
 
 
 def get_aggregated_potential(
