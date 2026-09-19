94d5f24a5096852118498560f7ceb360d0b0f0f8
guillecg <guillemcg92@gmail.com>
2026-09-19 15:24:29 +0200

    Add test_get_node_df_sample

--- tests/test_efd.py
+++ tests/test_efd.py
@@ -27,3 +27,25 @@ def test_get_node_df_genome(
             group_var=group_var
         )
     )
+
+
+def test_get_node_df_sample(
+    data_dir: str,
+    network_df_sample: pd.DataFrame,
+    redox_df: pd.DataFrame,
+    group_var: str = "sample_id"
+) -> None:
+    pd.testing.assert_frame_equal(
+        left=pd.read_csv(
+            os.path.join(
+                data_dir,
+                "results",
+                f"nodes-{group_var.split('_')[0]}.csv"
+            )
+        ),
+        right=get_node_df(
+            network_df=network_df_sample,
+            redox_df=redox_df,
+            group_var=group_var
+        )
+    )
