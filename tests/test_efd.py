2131c3c227cb713fbe6842a8b2777183f372652f
guillecg <guillemcg92@gmail.com>
2026-09-19 15:49:32 +0200

    Add test__get_node_colors_sample

--- tests/test_efd.py
+++ tests/test_efd.py
@@ -4,7 +4,16 @@ import os
 
 import pandas as pd
 
-from elektrapy.efd import get_node_df
+from elektrapy import PATHWAY_NODE_MAP, NODE_CYCLE_MAP, CYCLE_COLOR_MAP
+from elektrapy.efd import (
+    get_node_df,
+    get_efd,
+    get_rti_plot,
+    _get_node_colors,
+    _scale_nodes,
+    _encode_nodes,
+    _highlight_node
+)
 
 
 def test_get_node_df_genome(
@@ -48,4 +57,29 @@ def test_get_node_df_sample(
             redox_df=redox_df,
             group_var=group_var
         )
+    )
+
+
+def test__get_node_colors_sample(
+    data_dir: str,
+    node_df_sample: pd.DataFrame,
+    color_var: str,
+    cycle_color_map: dict = CYCLE_COLOR_MAP
+) -> None:
+
+    node_df = node_df_sample.copy()
+
+    node_df["node_color"] = node_df["node"].map(
+        _get_node_colors(cycle_color_map)
+    )
+
+    pd.testing.assert_frame_equal(
+        left=pd.read_csv(
+            os.path.join(
+                data_dir,
+                "results",
+                f"nodes-color-{color_var}.csv"
+            )
+        ),
+        right=node_df
     )
