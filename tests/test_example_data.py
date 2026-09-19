bec93e853b91409e1eaf449dd6f5b7bb73c9bfe9
guillecg <guillemcg92@gmail.com>
2026-09-19 11:11:18 +0200

    Add test_example_data.py

---
+++ tests/test_example_data.py
@@ -0,0 +1,61 @@
+import pytest
+
+import os
+
+import pandas as pd
+
+
+@pytest.fixture(scope="session", autouse=True)
+def metadata_df(data_dir) -> pd.DataFrame:
+    yield pd.read_csv(
+        os.path.join(
+            data_dir,
+            "metadata.csv"
+        )
+    )
+
+
+@pytest.fixture(scope="session", autouse=True)
+def pathway_df(data_dir) -> pd.DataFrame:
+    yield pd.read_table(
+        os.path.join(
+            data_dir,
+            "bigecyhmm",
+            "pathway_presence.tsv"
+        )
+    )
+
+
+def test_metadata_df_shape(metadata_df: pd.DataFrame) -> None:
+    assert metadata_df.shape == (12, 4), \
+        "[ERROR] Incorrect shape for metadata_df."
+
+
+def test_metadata_df_genome(metadata_df: pd.DataFrame) -> None:
+    assert "genome_id" in metadata_df.columns, \
+        "[ERROR] 'genome_id' column not in metadata_df."
+
+
+def test_metadata_df_sample(metadata_df: pd.DataFrame) -> None:
+    assert "sample_id" in metadata_df.columns, \
+        "[ERROR] 'sample_id' column not in metadata_df."
+
+
+def test_metadata_df_type(metadata_df: pd.DataFrame) -> None:
+    assert "type" in metadata_df.columns, \
+        "[ERROR] 'type' column not in metadata_df."
+
+
+def test_metadata_df_depth(metadata_df: pd.DataFrame) -> None:
+    assert "depth" in metadata_df.columns, \
+        "[ERROR] 'depth' column not in metadata_df."
+
+
+def test_pathway_df_shape(pathway_df: pd.DataFrame) -> None:
+    assert pathway_df.shape == (40, 13), \
+        "[ERROR] Incorrect shape for pathway_df."
+
+
+def test_pathway_df_function(pathway_df: pd.DataFrame) -> None:
+    assert "function" in pathway_df.columns, \
+        "[ERROR] 'function' column not in pathway_df."
