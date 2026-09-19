b897ea52fb6ac261857a9c62e39f77a282ea10a3
guillecg <guillemcg92@gmail.com>
2026-09-19 14:51:34 +0200

    Add test_get_redox_index_sample

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -47,3 +47,23 @@ def test_get_redox_index_genome(
             group_var=group_var
         ).reset_index(drop=True)
     )
+
+
+def test_get_redox_index_sample(
+    data_dir: str,
+    network_df_sample: pd.DataFrame,
+    group_var: str = "sample_id"
+) -> None:
+    pd.testing.assert_frame_equal(
+        left=pd.read_csv(
+            os.path.join(
+                data_dir,
+                "results",
+                f"indices-rti-{group_var.split('_')[0]}.csv"
+            )
+        ),
+        right=get_redox_index(
+            network_df=network_df_sample,
+            group_var=group_var
+        ).reset_index(drop=True)
+    )
