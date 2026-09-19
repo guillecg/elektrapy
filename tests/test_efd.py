f97a0cefa45ccbe4caecf458718ab0d2dbb3c92d
guillecg <guillemcg92@gmail.com>
2026-09-19 15:23:56 +0200

    Add test_get_node_df_genome

---
+++ tests/test_efd.py
@@ -0,0 +1,29 @@
+import pytest
+
+import os
+
+import pandas as pd
+
+from elektrapy.efd import get_node_df
+
+
+def test_get_node_df_genome(
+    data_dir: str,
+    network_df_genome: pd.DataFrame,
+    redox_df: pd.DataFrame,
+    group_var: str = "genome_id"
+) -> None:
+    pd.testing.assert_frame_equal(
+        left=pd.read_csv(
+            os.path.join(
+                data_dir,
+                "results",
+                f"nodes-{group_var.split('_')[0]}.csv"
+            )
+        ),
+        right=get_node_df(
+            network_df=network_df_genome,
+            redox_df=redox_df,
+            group_var=group_var
+        )
+    )
