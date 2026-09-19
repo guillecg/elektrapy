791b9cb7bd7254262b1ed867b6349449f4e9f14e
guillecg <guillemcg92@gmail.com>
2026-09-19 14:56:50 +0200

    Add test_get_mean_potential_genome

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -29,6 +29,16 @@ def network_df_sample(data_dir: str) -> pd.DataFrame:
     )
 
 
+@pytest.fixture(scope="module", autouse=False)
+def redox_df() -> pd.DataFrame:
+    redox_df = pd.read_csv("data/redox-potentials.csv")
+
+    # Skip first row containing the units
+    redox_df = redox_df.iloc[1:]
+
+    yield redox_df
+
+
 def test_get_redox_index_genome(
     data_dir: str,
     network_df_genome: pd.DataFrame,
@@ -65,5 +75,32 @@ def test_get_redox_index_sample(
         right=get_redox_index(
             network_df=network_df_sample,
             group_var=group_var
+        ).reset_index(drop=True)
+    )
+
+
+def test_get_mean_potential_genome(
+    data_dir: str,
+    network_df_genome: pd.DataFrame,
+    redox_df: pd.DataFrame,
+    group_var: str = "genome_id"
+) -> None:
+
+    rti_df = get_redox_index(
+        network_df=network_df_genome,
+        group_var=group_var
+    )
+
+    pd.testing.assert_frame_equal(
+        left=pd.read_csv(
+            os.path.join(
+                data_dir,
+                "results",
+                f"indices-redox-{group_var.split('_')[0]}.csv"
+            )
+        ),
+        right=get_mean_potential(
+            redox_df=redox_df,
+            rti_df=rti_df
         ).reset_index(drop=True)
     )
